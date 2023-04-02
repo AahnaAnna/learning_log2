46aae7cfd14b0cbcfa98570941f324b707b4ed89
Anna <aahnagulati@gmail.com>
2023-04-02 12:25:52 +0530

    ready for deployment of version 1

---
+++ learning_logs/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+app_name='learning_logs'
+
+urlpatterns=[
+    path('', views.index,name='index'),
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'),
+
+    #Detail page for a sigle topic
+    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
+
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+     # Page for adding a new topic
+    path('new_entry/<int:topic_id>/',views.new_entry, name='new_entry')
+
+]
